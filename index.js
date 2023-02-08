8dce68b278d5f7e8d6df285e39b50d020078b887
bayajidalam <bayajidalam2001@gmail.com>
2023-02-08 13:23:26 +0600

    Socket Event and Custom Event and Client to Server Data Transfer

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const { Server } = require("socket.io");
 const app = express();
 const port = process.env.PORT || 5000;
 
 const http = require('http');
-const server = http.createServer(app)
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
+
+io.on("connection", (socket)=>{
+  console.log("New user connected to our app")
+  socket.on("disconnect", (socket)=>{
+    console.log("User disconnect")
+  })
+
+  socket.on("testEvent", (data)=>{
+    console.log(data)
+  })
+})
 
 // home response 
 app.get('/', (req,res)=>{
-  res.send('Socket io Server is running')
+  res.sendFile(__dirname + '/app.html')
 })
 
-
 // check weather app is listening 
-app.listen(port, ()=>{
+httpServer.listen(port, ()=>{
   console.log(`app is listening on port${port}`)
 })
